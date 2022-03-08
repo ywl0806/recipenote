4978b12c489762e7e399e324f4a1dc11653fa14c
yongwoo <ywl0806@gmail.com>
2022-03-08 17:42:28 +0900

    fix

--- src/main/java/com/example/recipenote/config/WebConfig.java
+++ src/main/java/com/example/recipenote/config/WebConfig.java
@@ -1,24 +1,24 @@
-package com.example.recipenote.config;
-
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-@Configuration
-@EnableAutoConfiguration
-public class WebConfig implements WebMvcConfigurer {
-
-    @Value("${resource.path}")
-    private String resourcePath;
-
-    @Value("${upload.path}")
-    private String uploadPath;
-
-    @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler(uploadPath)
-                .addResourceLocations(resourcePath);
-    }
-}
+//package com.example.recipenote.config;
+//
+//import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//
+//@Configuration
+//@EnableAutoConfiguration
+//public class WebConfig implements WebMvcConfigurer {
+//
+//    @Value("${resource.path}")
+//    private String resourcePath;
+//
+//    @Value("${upload.path}")
+//    private String uploadPath;
+//
+//    @Override
+//    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+//        registry.addResourceHandler(uploadPath)
+//                .addResourceLocations(resourcePath);
+//    }
+//}
